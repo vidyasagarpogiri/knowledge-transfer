c02a55431d3bc39ae249f255dc22cb8e928ed2da
Ramarao <ramarao@ramarao.(none)>
2014-07-11 10:51:36 +0530

    pattabhi- db modification commited

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140710141325) do
-
 
   create_table "blogs", force: true do |t|
     t.string   "title"
