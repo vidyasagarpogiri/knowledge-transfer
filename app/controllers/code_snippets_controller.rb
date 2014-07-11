0ca6af78765ac0841fd361efbcd8c7617e5c59c5
Ramarao <ramarao@ramarao.(none)>
2014-07-11 13:28:29 +0530

    pattabhi- added codesnippet controller and views

--- app/controllers/code_snippets_controller.rb
+++ app/controllers/code_snippets_controller.rb
@@ -1,7 +1,37 @@
 class CodeSnippetsController < ApplicationController
 
   def index
+    @code_all = CodeSnippet.all  
+  end
   
+  def new
+    @code = CodeSnippet.new
+  end
+  
+  def create
+    @code= CodeSnippet.create(code_params)
+    redirect_to @code
+  end
+  
+  def show
+    #raise params.inspect
+    @code =CodeSnippet.find(params[:id])
+  end
+  
+  def edit
+    #raise params.inspect
+    @code = CodeSnippet.find(params[:id])
+  end
+  def update
+   @code = CodeSnippet.find(params[:id])
+   @code.update(code_params)
+   redirect_to @code
+  end
+  
+  private
+  
+  def code_params
+    params.require(:code_snippet).permit(:title, :content, :tags)
   end
   
 end
