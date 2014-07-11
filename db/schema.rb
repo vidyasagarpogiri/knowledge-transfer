e8cce341bcdc3d39cd0576ea5829aae5ec72620b
vidyasagar <vidyasagar@vidyasagar.(none)>
2014-07-11 20:28:00 +0530

    resolved conflicts issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140711143449) do
-
 
   create_table "answers", force: true do |t|
     t.text     "answer"
