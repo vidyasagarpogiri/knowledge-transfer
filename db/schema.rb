7f05eff561c0b9bbcb80aa16638a63c71e1de11d
Ramarao <ramarao@ramarao.(none)>
2014-07-11 14:52:15 +0530

    pattabhi - created answers model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711051934) do
+ActiveRecord::Schema.define(version: 20140711091732) do
+
+  create_table "answers", force: true do |t|
+    t.text     "answer"
+    t.string   "tags"
+    t.float    "answer_accept_rate", limit: 24
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
