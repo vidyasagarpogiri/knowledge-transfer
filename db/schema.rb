12b5e5c9b368737a5122f4d81b42dd602c609ecb
vidyasagar <vidyasagar@vidyasagar.(none)>
2014-07-11 13:48:28 +0530

    vidyasagar - resolved duplication of article and codesnippet headings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711045850) do
+ActiveRecord::Schema.define(version: 20140711051934) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
