d4f70e41109e0aeb08adb43dbe7ea26c705a2851
Abdul Rahaman <argoodfrnd2u@gmail.com>
2014-07-14 11:59:49 +0530

    Rahaman - resolved  articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,5 @@
 class Article < ActiveRecord::Base
-<<<<<<< HEAD
   include Bootsy::Container
-=======
  has_many :comments, as: :commentable
  belongs_to :category
->>>>>>> e8cce341bcdc3d39cd0576ea5829aae5ec72620b
 end
