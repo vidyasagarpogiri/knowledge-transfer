6db682663a79796174fbfda55ed70d0557a75dfc
vidyasagar <vidyasagar@vidyasagar.(none)>
2014-07-14 20:02:59 +0530

    vidyasagar - added category table to questions model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
          
   has_many :articles  
+  has_many :questions
          
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
