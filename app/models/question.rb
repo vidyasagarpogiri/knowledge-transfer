879a1167b37608bc944d95a872a3c85f1c10c320
vidyasagar <vidyasagar@vidyasagar.(none)>
2014-07-16 14:19:16 +0530

    vidyasagar - renamed code snippet to code base in all view files

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ActiveRecord::Base
   include Bootsy::Container
   belongs_to :category
   belongs_to :user
+  validates :title,  :presence => true
+  validates :content, :presence => true
+  
 end
