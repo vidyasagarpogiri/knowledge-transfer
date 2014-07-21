db38900a161801c93a0df32f491adec6641bbfed
Abdul Rahaman <argoodfrnd2u@gmail.com>
2014-07-21 18:22:09 +0530

    Search funcationality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,19 +5,31 @@ class SearchController < ApplicationController
     case params[:search_type]
       when "question"
           Question.search do
-             keywords params[:q]
+             fulltext params[:q] do
+              fields :title
+              minimum_match 1
+            end
           end.results
       when "article"
           Article.search do
-             keywords params[:q]
+             fulltext params[:q] do
+              fields :title
+              minimum_match 1
+            end
           end.results  
       when "blog"
           Blog.search do
-             keywords params[:q]
+             fulltext params[:q] do
+              fields :title
+              minimum_match 1
+            end
           end.results  
       when "code_snippet"
           CodeSnippet.search do
-             keywords params[:q]
+             fulltext params[:q] do
+              fields :title
+              minimum_match 1
+            end
           end.results
     end
   end
