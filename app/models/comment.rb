1750ceccff6f8f699a22d84846ca18749d19dc5b
sekharberi <sekhar.mca270@gmail.com>
2014-07-29 10:28:37 +0530

    sekhar - changes made in comment controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,22 +2,15 @@ class Comment < ActiveRecord::Base
   validates :content, presence: true
   belongs_to :commentable, polymorphic: true
   belongs_to :user
-  
-  
   after_create :user_points
   
   private 
   
   def user_points
-
-    commentable.comments.map(&:user_id).include?(user_id)
-
    count=Comment.select("user_id").where(:commentable_id =>self.commentable_id,:commentable_type=>self.commentable_type).count
    if count==1
     self.user.points += 5 if user_id?
     self.user.save!
    end
-    
   end
-  
 end
